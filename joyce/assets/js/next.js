c5bb008ac9b86fda2dbfd78922000d500b5ae85f
peonyxie <43351530+peonyxie@users.noreply.github.com>
2019-06-30 00:02:39 +0800

    Add files via upload

---
+++ joyce/assets/js/next.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$('#button').click(function(){
+		$("body").css("background-image","none");
+		$("body").css("background-color", 'white');
+		$('#button').css("display","none");
+		$('#click').css("display","none");
+		$("#night").css('display','none');
+		$('#drawing').css('display', 'block');
+		$('#words').css('display', 'block');
+		$('#words').css('font-color', '#1D1A46');
+		
+
+	})
+})
+
+
+
